28625ba2e29a22873121f41c834bc3912f2831f6
Infuseting <tuture27100@gmail.com>
2025-11-12 15:24:01 +0100

    Change / to /map/

---
+++ app/map/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import LeafletMap from "@/components/ui/leaflet-map";
+import UserMenu from "@/components/ui/user-menu";
+import LeftNavbar from "@/components/ui/left-navbar";
+import SearchBar from "@/components/ui/search-bar";
+import { LeftPanelProvider } from "@/components/ui/left-panel-context";
+import LeftPanel from "@/components/ui/left-panel";
+export default function MapPage() {
+  return (
+    <LeftPanelProvider>
+      <LeftPanel />
+      <LeftNavbar />
+      <SearchBar />
+
+      <LeafletMap />
+      <div className="fixed top-4 right-4 z-[9999] pointer-events-auto">
+        <UserMenu />
+      </div>
+    </LeftPanelProvider>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,5 @@
-import React from "react";
-import LeafletMap from "../components/ui/leaflet-map";
-import UserMenu from "@/components/ui/user-menu";
-import LeftNavbar from "../components/ui/left-navbar";
-import SearchBar from "../components/ui/search-bar";
-import { LeftPanelProvider } from "@/components/ui/left-panel-context";
-import LeftPanel from "@/components/ui/left-panel";
 export default function Home() {
   return (
-    <LeftPanelProvider>
-      <LeftPanel />
-      <LeftNavbar />
-      <SearchBar />
-
-      <LeafletMap />
-      <div className="fixed top-4 right-4 z-[9999] pointer-events-auto">
-        <UserMenu />
-      </div>
-    </LeftPanelProvider>
+    <h1>Main Page</h1>
   );
 }
